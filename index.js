412c0a1d09d461b9a2e9c8b69b16cdd94230007e
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-11-14 18:19:48 +0900

    add toggle animation

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const btn = document.querySelectorAll("button"),
-    personalInfo = document.querySelectorAll(".personal-info");
+const btn = document.querySelectorAll("button");
+
+
+function addClassList() {
+    const personalInfo = document.querySelectorAll(".personal-info");
+    for (j=0; j< personalInfo.length; j++) {
+        personalInfo[j].classList.toggle("hide");
+        personalInfo[j].classList.toggle("disappear");
+        personalInfo[j].classList.toggle("appear");
+    };
+};
 
 for (i=0; i< btn.length; i++) {
-    btn[i].addEventListener("click",function() {
-        for (j=0; j< personalInfo.length; j++) {
-            personalInfo[j].classList.toggle("hide");
-        }
-    });
+    btn[i].addEventListener("click", addClassList);
 };
